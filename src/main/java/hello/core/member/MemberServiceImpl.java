2b033576cded0ec34884ff2d1f6ac1f025633f8e
gyulchyoung <hacha2011@naver.com>
2022-01-24 15:55:54 +0900

    make appConfig

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,13 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
+    //DIP위반
+//    private final MemberRepository memberRepository= new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
